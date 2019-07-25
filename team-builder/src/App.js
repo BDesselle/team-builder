e7a8bda54bf7d2b906b163458eddc7560a32ad58
Brandon Desselle <desselle.brandon2@gmail.com>
2019-07-25 16:34:47 -0500

    Update App.js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import Form from './components/Form/Form';
+import { Button, Divider, Header, Segment, Card } from 'semantic-ui-react';
+import Formy from './components/Form/Formy';
 
 function App() {
   const [team, setTeam] = React.useState([]);
@@ -25,26 +26,46 @@ function App() {
     );
   };
 
+  const segStyle = {
+    width: '500px',
+    margin: '0 auto',
+  };
+
   return (
     <div className="App">
-      <h1>Team Builder App</h1>
-      <br></br>
-      <Form
+      <Header as='h1'>Team Builder</Header>
+      <Divider />
+      <Formy
         addMember={member => handleTeam(member)}
         editingMember={editingMember}
         editMember={member => editMember(member)}
       />
-      <br></br>
-      {team.map((member, index) => {
-        return (
-          <div key={index}>
-            <h2>Name: {member.name}</h2>
-            <h3>Email: {member.email}</h3>
-            <h3>Role: {member.role}</h3>
-            <button onClick={() => setMember(member)}>Edit</button>
-          </div>
-        );
-      })}
+      <div>
+        <Divider hidden />
+        <Segment style={segStyle} raised >
+          <Header as='h1'>Team List</Header>
+          {team.map((member, index) => {
+            return (
+              <div key={index}>
+                <Card.Group centered>
+                  <Card>
+                    <Card.Content>
+                      <Card.Header>{member.name}</Card.Header>
+                      <Card.Meta>{member.email}</Card.Meta>
+                      <Card.Description>
+                        {member.name} is a <strong>{member.role}</strong>.
+                      </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <Button basic color='red' onClick={() => setMember(member)}>Edit</Button>
+                    </Card.Content>
+                  </Card>
+                </Card.Group>
+              </div>
+            );
+          })}
+        </Segment>
+      </div>
     </div>
   );
 }
