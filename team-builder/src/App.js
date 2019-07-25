79575e09576dc0ff69936c2d18b0b8c6dd422575
Brandon Desselle <desselle.brandon2@gmail.com>
2019-07-25 16:41:45 -0500

    Update App.js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -31,41 +31,50 @@ function App() {
     margin: '0 auto',
   };
 
+  const container = {
+    width: '50%',
+    margin: '100px auto',
+  };
+
   return (
     <div className="App">
-      <Header as='h1'>Team Builder</Header>
-      <Divider />
-      <Formy
-        addMember={member => handleTeam(member)}
-        editingMember={editingMember}
-        editMember={member => editMember(member)}
-      />
-      <div>
+      <Segment style={container}>
         <Divider hidden />
-        <Segment style={segStyle} raised >
-          <Header as='h1'>Team List</Header>
-          {team.map((member, index) => {
-            return (
-              <div key={index}>
-                <Card.Group centered>
-                  <Card>
-                    <Card.Content>
-                      <Card.Header>{member.name}</Card.Header>
-                      <Card.Meta>{member.email}</Card.Meta>
-                      <Card.Description>
-                        {member.name} is a <strong>{member.role}</strong>.
-                      </Card.Description>
-                    </Card.Content>
-                    <Card.Content extra>
-                      <Button basic color='red' onClick={() => setMember(member)}>Edit</Button>
-                    </Card.Content>
-                  </Card>
-                </Card.Group>
-              </div>
-            );
-          })}
-        </Segment>
-      </div>
+        <Header as='h1'>Team Builder</Header>
+        <Divider hidden />
+        <Formy
+          addMember={member => handleTeam(member)}
+          editingMember={editingMember}
+          editMember={member => editMember(member)}
+        />
+        <div>
+          <Divider hidden />
+          <Segment style={segStyle} raised >
+            <Header as='h1'>Team List</Header>
+            {team.map((member, index) => {
+              return (
+                <div key={index}>
+                  <Card.Group centered>
+                    <Card>
+                      <Card.Content>
+                        <Card.Header>{member.name}</Card.Header>
+                        <Card.Meta>{member.email}</Card.Meta>
+                        <Card.Description>
+                          {member.name} is a <strong>{member.role}</strong>.
+                        </Card.Description>
+                      </Card.Content>
+                      <Card.Content extra>
+                        <Button basic color='red' onClick={() => setMember(member)}>Edit</Button>
+                      </Card.Content>
+                    </Card>
+                  </Card.Group>
+                </div>
+              );
+            })}
+          </Segment>
+        </div>
+        <Divider hidden />
+      </Segment>
     </div>
   );
 }
