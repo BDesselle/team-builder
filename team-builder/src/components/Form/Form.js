b8612702c4d64b684a28664e688a030555073824
Brandon Desselle <desselle.brandon2@gmail.com>
2019-07-25 12:35:45 -0500

    Update Form.js

--- team-builder/src/components/Form/Form.js
+++ team-builder/src/components/Form/Form.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Form = ({ addMember }) => {
+const Form = ({ addMember, editingMember, editMember }) => {
   const [member, setMember] = React.useState({
     name: '',
     email: '',
@@ -12,45 +12,131 @@ const Form = ({ addMember }) => {
     setMember({ ...member, [name]: value })
   }
 
-  const handleSubmit = (submit) => {
+  React.useEffect(() => {
+    if (editingMember) {
+      setMember(editingMember)
+    }
+  }, [editingMember])
+
+  const addTeamMember = (add) => {
     addMember(member);
-    submit.preventDefault();
+    setMember({
+      name: '',
+      email: '',
+      role: '',
+    });
+    add.preventDefault()
   }
 
-  return (
-    <React.Fragment>
-      <h3>Create A Team Member</h3>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name
+  const editTeamMember = (edit) => {
+    editMember(member);
+    setMember({
+      name: '',
+      email: '',
+      role: '',
+    });
+    edit.preventDefault()
+  }
+
+  const { name, email, role } = member;
+
+  if (editingMember) {
+    return (
+      <React.Fragment>
+        <h3>Create A Team Member</h3>
+        <form onSubmit={editTeamMember}>
+          <label>
+            Name
+            <div>
+              <input
+                name='name'
+                placeholder='Name'
+                value={name}
+                onChange={(change) => handleChange(change)}
+              />
+            </div>
+          </label>
+          <label>
+            Email
+            <div>
+              <input
+                name='email'
+                placeholder='Email'
+                value={email}
+                onChange={(change) => handleChange(change)}
+              />
+            </div>
+          </label>
+          <label>
+            Role
+            <div>
+              <input
+                name="role"
+                value={role}
+                onChange={(event) => handleChange(event)}
+              />
+              {/* <select name='name' placeholder='Role' onChange={(change) => handleChange(change)}>
+                <option value="0">Fullstack Developer</option>
+                <option value="1">Backend Developer</option>
+                <option value="2">Frontend Developer</option>
+                <option value="3">UI/UX Designer</option>
+              </select> */}
+            </div>
+          </label>
+          <br></br>
           <div>
-            <input name='name' placeholder='Name' onChange={(change) => handleChange(change)} />
+            <input type="submit" value="Submit" />
           </div>
-        </label>
-        <label>
-          Email
+        </form>
+      </React.Fragment>
+    );
+  }
+  else {
+    return (
+      <React.Fragment>
+        <h3>Create A Team Member</h3>
+        <form onSubmit={addTeamMember}>
+          <label>
+            Name
+            <div>
+              <input
+                name='name'
+                placeholder='Name'
+                value={name}
+                onChange={(change) => handleChange(change)}
+              />
+            </div>
+          </label>
+          <label>
+            Email
+            <div>
+              <input
+                name='email'
+                placeholder='Email'
+                value={email}
+                onChange={(change) => handleChange(change)}
+              />
+            </div>
+          </label>
+          <label>
+            Role
+            <div>
+              <input
+                name="role"
+                placeholder='Role'
+                value={role}
+                onChange={(event) => handleChange(event)}
+              />
+            </div>
+          </label>
+          <br></br>
           <div>
-            <input name='email' placeholder='Email' onChange={(change) => handleChange(change)} />
+            <input type="submit" value="Submit" />
           </div>
-        </label>
-        <label>
-          Role
-          <div>
-            <select name='name' placeholder='Role' onChange={(change) => handleChange(change)}>
-              <option value="fsDev">Fullstack Developer</option>
-              <option value="beDev">Backend Developer</option>
-              <option value="feDev">Frontend Developer</option>
-              <option value="uiux">UI/UX Designer</option>
-            </select>
-          </div>
-        </label>
-        <br></br>
-        <div>
-          <input type="submit" value="Submit" />
-        </div>
-      </form>
-    </React.Fragment>
-  );
+        </form>
+      </React.Fragment>
+    );
+  }
 };
 
 export default Form;
